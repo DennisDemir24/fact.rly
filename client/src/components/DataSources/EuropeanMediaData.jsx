c607ea2eb5791d78692f4fc45243f5001de176e7
Dennis Demir <dd222gc@student.lnu.se>
2022-03-09 08:59:06 +0100

    Adding icons to media data items

--- client/src/components/DataSources/EuropeanMediaData.jsx
+++ client/src/components/DataSources/EuropeanMediaData.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react'
 import {data} from '../../TempData/data'
+import {HiClipboardCopy} from 'react-icons/hi'
+import {FiExternalLink} from 'react-icons/fi'
 
 const EuropeanMediaData = () => {
 
@@ -12,9 +14,14 @@ const EuropeanMediaData = () => {
     <>
         {data.map((item, index) => {
             return (
-                <div key={item.id} className='flex flex-col pt-2 px-2 pb-2 w-6/12'>
-                    <h2 className='font-bold'>{item.title}</h2>
-                </div>
+              <div
+                key={item.id}
+                className='flex flex-row pt-2 px-2 pb-2 w-6/12'
+              >
+                <h2 className='font-bold'>{item.title}</h2>
+                  <FiExternalLink className="ml-1" />
+                  <HiClipboardCopy className="ml-1" />
+              </div>
             )
         })}
     </>
